e6ac53522961aa736b386a330eb82ea33b2fac57
Clem Freeman <clemfree@gmail.com>
2015-01-23 14:49:18 -0800

    Create add stylist form to index.erb

--- app.rb
+++ app.rb
@@ -8,6 +8,11 @@ require './lib/stylist'
 DB = PG.connect({:dbname => 'hair_salon'})
 
 get '/' do
-  stylists = Stylist.all
+  @stylists = Stylist.all
   erb :index
+end
+
+post '/addstylist' do
+  Stylist.new(params.fetch("name"), nil).save
+  redirect '/'
 end
