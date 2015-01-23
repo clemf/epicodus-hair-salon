a134e5d3bd9e7d24a4f7e3dad8aca1c0e84bdae7
Clem Freeman <clemfree@gmail.com>
2015-01-23 12:52:55 -0800

    Add basic logic for Sinatra app

---
+++ app.rb
@@ -0,0 +1,12 @@
+require 'sinatra'
+require 'sinatra/reloader'
+also_reload('./lib/**/*.rb')
+require 'pg'
+require './lib/client'
+require './lib/stylist'
+
+DB = PG.connect({:dbname => 'hair_salon'})
+
+get '/' do
+  erb :index
+end
